dba9f6fa3f0cead0f0380751f28fe3b62c23a407
16BrooksA <16BrooksA@clevedonschool.org.uk>
2022-11-25 10:46:27 +0000

    Rotational Movement
    arrow keys now control rotation in yaw and pitch axis using AddTorque() function. Also, up and down movement with space and left ctrl keys implemented.

--- Spaceflight/Assets/Spaceflight.cs
+++ Spaceflight/Assets/Spaceflight.cs
@@ -4,28 +4,42 @@ using UnityEngine;
 
 public class Spaceflight : MonoBehaviour
 {
-    float horizontalMovement;
-    float verticalMovement;
+    public float horizontalMovement;
+    public float verticalMovement;
+    public float jumpMovement;
+    public float horizontalRotation;
+    public float verticalRotation;
 
-    Vector3 moveDirection;
+    // Vector3 takes value for x, y, z axis
+    public Vector3 moveDirection;
+    public Vector3 rotateDirection;
 
     Rigidbody rigidbody;
 
+    // Start is called before the first frame update
     void Start()
     {
+        // Asigns the rigidbody to my object
         rigidbody = GetComponent<Rigidbody>();
     }
 
+    // Update is called once per frame
     void Update()
     {
         horizontalMovement = Input.GetAxisRaw("Horizontal");
         verticalMovement = Input.GetAxisRaw("Vertical");
+        jumpMovement = Input.GetAxisRaw("Jump");
+        horizontalRotation = Input.GetAxisRaw("Horizontal2");
+        verticalRotation = Input.GetAxisRaw("Vertical2");
 
-        moveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
+        moveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement + transform.up * jumpMovement;
+        rotateDirection = transform.up * horizontalRotation + transform.right * verticalRotation;
     }
     
+    // FixedUpdate is called 50 times per second
     void FixedUpdate()
     {
         rigidbody.AddForce(moveDirection);
+        rigidbody.AddTorque(rotateDirection);
     }
 }
