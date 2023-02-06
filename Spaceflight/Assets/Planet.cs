806336b0be1ab4e9f870e75ff1e608d3d665e976
16BrooksA <16BrooksA@clevedonschool.org.uk>
2023-02-06 12:48:27 +0000

    Calculating Gravity (WIP)

--- Spaceflight/Assets/Planet.cs
+++ Spaceflight/Assets/Planet.cs
@@ -1,16 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    public float mass;
+    public Decimal planet_mass = 5.9722e24m;
+    public Decimal gravitational_constant = 6.6743e-11m;
+    public float gravitational_force;
+    public Decimal distance;
+    public Decimal ship_mass;
 
     Rigidbody rigidbody;
 
     void Start()
     {
+        GameObject Planet1 = GameObject.Find("Planet1");
+
+        GameObject spaceship = GameObject.Find("Spaceship");
+        Spaceship ship = spaceship.GetComponent<Spaceship>();
+        Decimal ship_mass = Convert.ToDecimal(ship.spaceship_mass);
+
+        Decimal distance = Convert.ToDecimal(Vector3.Distance(spaceship.transform.position, Planet1.transform.position));
+
         // Asigns the rigidbody to my object
         rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        //Decimal result = Decimal.Multiply(gravitational_constant, planet_mass);
+        gravitational_force = (float)((gravitational_constant * planet_mass * ship_mass) / (distance * distance));
+
     }
 }
--- Spaceflight/Assets/Spaceship.cs
+++ Spaceflight/Assets/Spaceship.cs
@@ -4,20 +4,11 @@ using UnityEngine;
 
 public class Spaceship : MonoBehaviour
 {
+    public float spaceship_mass = 1000;
+
     void Start()
     {
-        GameObject Planet1 = GameObject.Find("Planet1");
-        Planet planet = Planet1.GetComponent<Planet>();
-        float planet_mass = planet.mass;
-
-        if(planet_mass == 100)
-        {
-            print("yes");
-        }
-        else
-        {
-            print("no");
-        }
+        
     }
     
 }
