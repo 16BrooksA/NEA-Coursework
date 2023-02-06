1af15e5b8f8387350f80d7ca55ef0809fe2f5177
16BrooksA <16BrooksA@clevedonschool.org.uk>
2023-02-06 19:55:10 +0000

    Calculating Gravity

--- Spaceflight/Assets/Planet.cs
+++ Spaceflight/Assets/Planet.cs
@@ -8,20 +8,25 @@ public class Planet : MonoBehaviour
     public Decimal planet_mass = 5.9722e24m;
     public Decimal gravitational_constant = 6.6743e-11m;
     public float gravitational_force;
-    public Decimal distance;
+    public float distance;
+    public Decimal dec_distance;
     public Decimal ship_mass;
+
+    public GameObject Planet1;
+    public GameObject spaceship;
 
     Rigidbody rigidbody;
 
     void Start()
     {
-        GameObject Planet1 = GameObject.Find("Planet1");
+        Planet1 = GameObject.Find("Planet1");
 
-        GameObject spaceship = GameObject.Find("Spaceship");
+        spaceship = GameObject.Find("Spaceship");
         Spaceship ship = spaceship.GetComponent<Spaceship>();
-        Decimal ship_mass = Convert.ToDecimal(ship.spaceship_mass);
+        ship_mass = Convert.ToDecimal(ship.spaceship_mass);
 
-        Decimal distance = Convert.ToDecimal(Vector3.Distance(spaceship.transform.position, Planet1.transform.position));
+        /*float distance = Vector3.Distance(spaceship.transform.position, Planet1.transform.position);
+        Decimal dec_distance = Convert.ToDecimal(distance);*/
 
         // Asigns the rigidbody to my object
         rigidbody = GetComponent<Rigidbody>();
@@ -29,8 +34,11 @@ public class Planet : MonoBehaviour
 
     void FixedUpdate()
     {
+        distance = Vector3.Distance(spaceship.transform.position, Planet1.transform.position);
+        dec_distance = Convert.ToDecimal(distance);
+
         //Decimal result = Decimal.Multiply(gravitational_constant, planet_mass);
-        gravitational_force = (float)((gravitational_constant * planet_mass * ship_mass) / (distance * distance));
+        gravitational_force = (float)((gravitational_constant * planet_mass * ship_mass) / (dec_distance * dec_distance));
 
     }
 }
